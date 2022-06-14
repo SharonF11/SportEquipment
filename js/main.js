d9c5a37b34afd5c9067c9a034b695b62c85962a1
SharonF11 <sharon@fungalaxy.ie>
2022-06-14 20:53:40 +0100

    zindex

--- js/main.js
+++ js/main.js
@@ -25,8 +25,8 @@ function showSlides(n) {
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
+(  }
+)  for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block"; 
